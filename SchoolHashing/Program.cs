918077d2f09311103192e22ac72e8ed0e723caad
Benjamin Larsen <beela93@hotmail.com>
2023-03-03 10:33:39 +0100

    forgot to save file

--- SchoolHashing/Program.cs
+++ SchoolHashing/Program.cs
@@ -1,15 +1,5 @@
 ﻿using SchoolHashing;
 
-//string hashed1 = Hasher.HashAndSalt("Test");
-//string hashed2 = Hasher.HashAndSalt("Test");
-//string hashed3 = Hasher.HashAndSalt("This is a test");
-//string hashed4 = Hasher.HashAndSalt("This is a test");
-//Console.WriteLine(hashed1);
-//Console.WriteLine(hashed2);
-//Console.WriteLine(hashed3);
-//Console.WriteLine(hashed4);
-
-
 var context = new Context();
 context.Registrate("Bob", "Test123!");
 context.Registrate("Bob The Second", "Test123!");
